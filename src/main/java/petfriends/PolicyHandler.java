39dbbbe8a8fe5649debc3ddeca7e1bd2f5ece168
SKCC\08300 <minahso@sk.com>
2022-08-30 11:21:22 +0900

    point지급 수정

--- src/main/java/petfriends/PolicyHandler.java
+++ src/main/java/petfriends/PolicyHandler.java
@@ -1,6 +1,7 @@
 package petfriends;
 
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,52 +41,72 @@ public class PolicyHandler{
     	// TODO :
     	// 1.포인트 : 책정은 일단 결제 액의 10% -> 결제금액은 reservedId로 조회하여 amount가져옴.
     	// 2.현재포인트 : 회원정보에서 가져와야하는데 임시로 포인트테이블에서 사용자이름으로 최종 포인트를 찾아옴.
+    	String dogWalkerId = "";
+    	String userId ="";
+    	Double amount = (double)0;
+    	
+    	Double earnPoint =(double)0;
+    	Double userCurrentPoint = (double)0;
+    	Double userUpdateCurrentPoint = (double)0;
+    	
+    	Double dwCurrentPoint = (double)0;
+    	Double dwEarnPoint = (double)0;
+    	Double dwUpdateCurrentPoint = (double)0;
+    	
     	if(walkEnded.isMe()){
             System.out.println("######## walkEndede listener  : " + walkEnded.toJson());
-           
-            //결제금액 
-            Double amount = paymentService.getAmount(walkEnded.getReservedId());
-        
-            //댕주인 지급포인트
-            Double earnPoint = amount * (double)0.1 ; //결제금액의 10%
+ 
+            userId = walkEnded.getUserId(); //dogWalkerId
+            amount = paymentService.getAmount(walkEnded.getReservedId()); //결제금액
+            earnPoint = amount * 0.1 ; //사용자포인트(지급금액의 10%)
+            
             //댕주인 현재포인트
-            List<Point> userPointList = paymentService.findPointAllByUserId(walkEnded.getUserId());
-            Double currentPoint = (double)0;
+            List<Point> userPointList = paymentService.findPointAllByUserId(userId);
             if(!userPointList.isEmpty()) {
-            	currentPoint = userPointList.get(userPointList.size()-1).getCurrentPoint() + earnPoint;
+            	userCurrentPoint = userPointList.get(userPointList.size()-1).getCurrentPoint();
             }
-            //댕주인 저장데이터
+            
+            userUpdateCurrentPoint = userCurrentPoint + earnPoint ; //새로추가될 포인트
+            
+            System.out.println("userId: "+ userId);
+            System.out.println("amount:" + amount + "user현재포인트: "  + userCurrentPoint + ", 지급Point: " + earnPoint);
+            System.out.println("user계산된현재포인트: " + userUpdateCurrentPoint);
+            
+
+            //댕주인 point데이터
             Point userPoint = new Point();     		
             userPoint.setCreateDate(new Timestamp(System.currentTimeMillis()));
-            userPoint.setCurrentPoint(currentPoint);
             userPoint.setPoint(earnPoint);
+            userPoint.setCurrentPoint(userUpdateCurrentPoint);
             userPoint.setPointGubun(PointGubun.EARN);
             userPoint.setReservedId(walkEnded.getReservedId());
-            userPoint.setUserId(walkEnded.getUserId());
-            //댕주인 point지급 저장
-            pointRepository.save(userPoint);  
+            userPoint.setUserId(userId);
             
-            
-            //도그워커 지급포인트
-            Double dwEarnPoint = amount; //결제금액
-            //댕주인 현재포인트
-            List<Point> dwPointList = paymentService.findPointAllByUserId(walkEnded.getDogWalkerId());
-            Double dwCurrentPoint = (double)0;
+            dogWalkerId = walkEnded.getDogWalkerId(); //dogWalkerId
+            dwEarnPoint = amount; //도그워커 지급포인트
+            //dogwalker 현재포인트
+            List<Point> dwPointList = paymentService.findPointAllByUserId(dogWalkerId);
             if(!dwPointList.isEmpty()) {
-            	dwCurrentPoint = dwPointList.get(dwPointList.size()-1).getCurrentPoint() + dwEarnPoint;
+            	dwCurrentPoint = dwPointList.get(dwPointList.size()-1).getCurrentPoint();
             }
             
-            //도그워커 저장데이터
+            dwUpdateCurrentPoint = userCurrentPoint + earnPoint ; //새로계산된 현재포인트
+            
+            System.out.println("dogWalkerId: "+ userId);
+            System.out.println("amount:" + amount + "dogwalker현재포인트: "  + dwCurrentPoint + ", 지급Point: " + dwEarnPoint);
+            System.out.println("dogwalker계산된현재포인트: " + dwUpdateCurrentPoint);
+            
+            //도그워커 point데이터
             Point dwPoint = new Point();     		
             dwPoint.setCreateDate(new Timestamp(System.currentTimeMillis()));
-            dwPoint.setCurrentPoint(dwCurrentPoint);
             dwPoint.setPoint(dwEarnPoint);
+            dwPoint.setCurrentPoint(dwUpdateCurrentPoint);
             dwPoint.setPointGubun(PointGubun.WAGE);
             dwPoint.setReservedId(walkEnded.getReservedId());
-            dwPoint.setUserId(walkEnded.getDogWalkerId());
+            dwPoint.setUserId(dogWalkerId);
             
-            //도그워커 point(요금)지급 저장
-            pointRepository.save(dwPoint);  
+            List<Point> points = Arrays.asList(userPoint, dwPoint);
+            pointRepository.saveAll(points); //user, 도그워커 point(요금)지급 저장
             
         }
     }
